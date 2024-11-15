6ba3fab9e622fb606bc5a45fef6918a342c232be
Kalmuk Yaropolk <yarko.kalmuk@gmail.com>
2024-11-15 22:50:24 +0200

    starting commit

---
+++ main.py
@@ -0,0 +1,36 @@
+"""main.py module"""
+import argparse
+
+def read_file(filename: str) :
+    """
+    Reads a csv file and returns ...
+    """
+    pass
+
+
+def unconnected_places() :
+    """
+    Function finds unconnected places
+    """
+    pass
+
+
+def shortest_connection() :
+    """
+    Function finds the shortest ways to restore
+    connection
+    """
+    pass
+
+def terminal_display():
+    """
+    A function that will allow user to run module in terminal
+    """
+    pass
+
+def ui_design():
+    """
+    A function to make a pretty ui design
+    """
+    pass
+
