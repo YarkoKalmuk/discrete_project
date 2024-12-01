7d18bb44818fb7ece9b3614adb7483456723e3ec
Kalmuk Yaropolk <yarko.kalmuk@gmail.com>
2024-12-01 15:01:05 +0200

    main adde

--- main.py
+++ main.py
@@ -1,47 +1,83 @@
-# """main.py module"""
-# import argparse
+"""main.py module"""
+import argparse
 
-# def read_file(filename: str) -> list[ dict[tuple[str, str]: set[tuple[str, str, int]]],
-#                                      set[tuple[tuple[str, str], tuple[str, str], int]]]:
-#     """
-#     Reads a csv file and returns a list with first el as dictionary with keys as tuples
-#     containing type of place and its name and values as set of connected
-#     places as tuples of type of place and its name and int as a road lenght, 
-#     and second element in list is set of blocked roads.
+def read_file(filename: str) -> list[ dict[tuple[str, str]: set[tuple[str, str, int]]],
+                                     set[tuple[tuple[str, str], tuple[str, str], int]]]:
+    """
+    Reads a csv file and returns a list with first el as dictionary with keys as tuples
+    containing type of place and its name and values as set of connected
+    places as tuples of type of place and its name and int as a road lenght, 
+    and second element in list is set of blocked roads.
 
-#     :param filename: str, A file name to read from
+    :param filename: str, A file name to read from
 
-#     :return:list[ dict[tuple[str, str], set[ tuple[tuple[str, str], tuple[str, str]]]], where
-#         - first el of list is a dictionary with type and name of place as keys and
-#     type, name of connected places and distance between them as values,
-#         - second el is a set of blocked roads, as tuples of tuple of first
-#     place and its type and another tuple as second place, its name and lenght of the road.
+    :return:list[ dict[tuple[str, str], set[ tuple[tuple[str, str], tuple[str, str]]]], where
+        - first el of list is a dictionary with type and name of place as keys and
+    type, name of connected places and distance between them as values,
+        - second el is a set of blocked roads, as tuples of tuple of first
+    place and its type and another tuple as second place, its name and lenght of the road.
 
-#     Example:
+    Example:
 
-#     Зв'язки:
-# село A, місто B, 10
-# місто B, обласний центр D, 15
-# село A, село C, 5
+Connections:
+city A, village B, 1
+village B, regional_center C, 1
+regional_center C, village F, 5
+village F, city E, 1
+city E, village D, 1
+village D, city G, 2
+city G, village H, 1
+village H, city I, 1
+city I, city A, 2
 
-# Заблоковані дороги:
-# село A, місто B, 10
-# село C, обласний центр D, 15
+Blocked roads:
+city I, city A, 2
+village D, city G, 2
+regional_center C, village F, 5
 
-#     Output:
-# [
-#     {
-#         ("village", "A"): {("city", "B", 10), ("village", "C", 5)},
-#         ("city", "B"): {("regional_center", "D", 15)},
-#         ("village", "C"): ("village", "A", 5)
-#     },
-#     {
-#         (("village", "A"), ("city", "B"), 10),
-#         (("village", "C"), ("regional_center", "D"), 15),
-#     }
-# ]
-#     """
-#     pass
+
+    Output:
+[
+    {
+        ("village", "A"): {("city", "B"), ("village", "C")},
+        ("city", "B"): {("regional_center", "D")},
+        ("village", "C"): ("village", "A")
+    },
+    {
+        (("village", "A"), ("city", "B"), 10),
+        (("village", "C"), ("regional_center", "D"), 15),
+    }
+]
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        blocked = set()
+        all_road = {}
+        status_road = True
+        for i in file:
+            line = i.strip("\n").split()
+            if line == []:
+                continue
+            if "Blocked" in line:
+                status_road = False
+                continue
+            elif "Connections:" in line:
+                continue
+            key1 = (line[0], line[1][:-1])
+            key2 = (line[2], line[3][:-1])
+            if status_road:
+                if key1 not in all_road:
+                    all_road[key1] = {(key2)}
+                else:
+                    all_road[key1].add(key2)
+                if key2 not in all_road:
+                    all_road[key2] = {(key1)}
+                else:
+                    all_road[key2].add(key1)
+            else:
+                block = (key1, key2, int(line[4]))
+                blocked.add(block)
+        result = [all_road, blocked]
+        return result
 
 
 def unconnected_places(
@@ -109,64 +145,89 @@ def unconnected_places(
     return components
 
 
-
-# def shortest_connection(paths: dict[tuple[str,str]: set[tuple[tuple[str, str], 15]]],
-#                         blocked: set[tuple[tuple[str, str], tuple[str, str], int]])\
-#                         -> set[tuple[tuple[str, str], tuple[str, str], int]]:
-#     """
-#     Finds a way to connect all points to the regional center as cheap
-#     (cost measured in km of roads restored) as possible.
-#     Input:
-#     {
-#         ("village", "A"): {("city", "B"), ("village", "C")},
-#         ("city", "B"): {("regional_center"), ("city", "A")},
-#         ("village", "C"): {("village", "A"), ("regional_center", "D")},
-#         ("regional_center", "D"): {("city", "B"), ("village", "C")}
-#     },
-#     {
-#         (("village", "A"), ("city", "B"), 10)
-#         (("regional_center", "D"), ("village", "C"), 8)
-#     }
-
-#     Output:
-#     {
-#         (("regional_center", "D"), ("village", "C"), 8)
-#     }
-#     """
-#     blocked = blocked.copy()
-#     restored = set()
-#     while len(blocked) > 0:
-#         disconnected, groups = unconnected_places(paths, blocked)
-#         accessible = groups[0]
-#         options = []
-#         for node in accessible:
-#             for vertice in disconnected:
-#                 if vertice in paths[node]:
-#                     for block in blocked:
-#                         if node in block and vertice in block:
-#                             options.append(block)
-#         options = sorted(options, key=lambda x: x[2])
-#         choice = options[0]
-#         blocked.remove(choice)
-#         restored.add(choice)
-#     return restored
-
-
-# def write_to_file(filename : str ) -> None: # plus two arguments as a result of func unconnected_places and shortest_places
-#     """
-#     Function writes output to a file
-
-#     :param filename: str, A name of the file you want to write to
-#     :param:
-#     :param:
-
-#     :return : None
-#     """
-#     pass
-
-# allows user to run module in terminal
+def shortest_connection(paths: dict[tuple[str,str]: set[tuple[tuple[str, str]]]],
+                        blocked: set[tuple[tuple[str, str], tuple[str, str], int]])\
+                        -> set[tuple[tuple[str, str], tuple[str, str], int]]:
+    """
+    Finds a way to connect all points to the regional center as cheap
+    (cost measured in km of roads restored) as possible.
+    >>> shortest_connection({\
+        ("village", "A"): {("city", "B"), ("village", "C")},\
+        ("city", "B"): {("regional_center"), ("city", "A")},\
+        ("village", "C"): {("village", "A"), ("regional_center", "D")},\
+        ("regional_center", "D"): {("city", "B"), ("village", "C")}\
+    },{(("village", "A"), ("city", "B"), 10),\
+        (("regional_center", "D"), ("village", "C"), 8)})
+    {(('regional_center', 'D'), ('village', 'C'), 8)}
+    >>> shortest_connection({\
+        ("city", "A"): {("city", "B"), ("regional_center", "C"), ("city", "I")},\
+        ("city", "B"): {("city", "A"), ("regional_center", "C")},\
+        ("regional_center", "C"): {("city", "B"), ("city", "A"), ("city", "F")},\
+        ("city", "H"): {("city", "G"), ("city", "I")},\
+        ("city", "G"): {("city", "H"), ("city", "I"), ("city", "D")},\
+        ("city", "I"): {("city", "G"), ("city", "H"), ("city", "A")},\
+        ("city", "E"): {("city", "D"), ("city", "F")},\
+        ("city", "F"): {("city", "D"), ("city", "E"), ("regional_center", "C")},\
+        ("city", "D"): {("city", "F"), ("city", "E"), ("city", "G")},\
+    },{(("city", "I"), ("city", "A"), 3),\
+    (("city", "G"), ("city", "D"), 5),\
+    (("city", "E"), ("city", "F"), 1),\
+    (("city", "A"), ("city", "C"), 2),\
+    (("regional_center", "C"), ("city", "F"), 3)}) == {\
+    (("regional_center", "C"), ("city", "F"), 3), (("city", "I"), ("city", "A"), 3)}
+    True
+    """
+    blocked = blocked.copy()
+    restored = set()
+    while True:
+        reblocked = {(a, b) for a, b, c in blocked}
+        groups = unconnected_places(paths, reblocked)
+        if len(groups) > 1:
+            disconnected = set.union(*groups[1:])
+        else:
+            break
+        accessible = groups[0]
+        options = []
+        for node in accessible:
+            for vertice in disconnected:
+                if vertice in paths[node]:
+                    for block in blocked:
+                        if node in block and vertice in block:
+                            options.append(block)
+        options = sorted(options, key=lambda x: x[2])
+        choice = options[0]
+        blocked.remove(choice)
+        restored.add(choice)
+    return restored
 
 
-if __name__ == "__main__":
+def write_to_file(filename: str, unconnected: list[set[tuple[str, str]]], restored: set[tuple[tuple[str, str], tuple[str, str], int]]) -> None:
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write("Незв'язані місця:\n")
+        for bobik in unconnected:
+            file.write(f"{', '.join([f'{place[0]} {place[1]}' for place in bobik])}\n")
+        file.write("\nВідновлені дороги:\n")
+        for road in restored:
+            punkt1, punkt2, length = road
+            file.write(f"{punkt1[0]} {punkt1[1]}, {punkt2[0]} {punkt2[1]}, {length}\n")
+
+
+def main(input_filename:str, output_filename:str) -> None:
+    """
+    A main function
+
+    :param input_f:str, A file to read from
+    :param output_f:str, A file to write to
+    :return: None
+    """
+    all_roads, blocked_roads = read_file(input_filename)
+    blocked_no_weight = {(road[0], road[1]) for road in blocked_roads}
+    unconnected = unconnected_places(all_roads, blocked_no_weight)
+    restored = shortest_connection(unconnected, blocked_roads)
+    write_to_file(output_filename, unconnected, restored)
+
+main("input_example.csv", "output_example.csv")
+
+if __name__ == '__main__':
     import doctest
-    print(doctest.testmod())
+    doctest.testmod()
